383bfc48cc95510659d9174c2fb264a5bc0af0e4
Antony Njoroge <antonjoro2008@gmail.com>
2020-02-17 17:29:46 +0300

    jackpot bet placement

--- app/controllers/JackpotController.php
+++ app/controllers/JackpotController.php
@@ -9,7 +9,7 @@ class JackpotController extends ControllerBase
         $total_games = $jackpotID['0']['total_games'];
         $jackpotID = $jackpotID['0']['jackpot_event_id'];
 
-    	$games =  "select j.game_order as pos, jackpot_match_id,e.sub_type_id, group_concat(concat(odd_value)) as threeway, m.game_id, m.match_id, m.start_time, m.parent_match_id, m.away_team, m.home_team, c.competition_name, c.category from jackpot_match j inner join `match` m on m.parent_match_id = j.parent_match_id INNER JOIN competition c ON m.competition_id = c.competition_id inner join event_odd e on e.parent_match_id = m.parent_match_id where j.jackpot_event_id=? and j.status='ACTIVE'  and e.sub_type_id=1 group by j.parent_match_id order by game_order";
+    	$games =  "select j.game_order as pos, jackpot_match_id,e.sub_type_id, group_concat(concat(odd_value)) as threeway, m.game_id, m.match_id, m.start_time, m.parent_match_id, m.away_team, m.home_team, c.competition_name, c.category from jackpot_match j inner join `match` m on m.parent_match_id = j.parent_match_id INNER JOIN competition c ON m.competition_id = c.competition_id inner join event_odd e on e.parent_match_id = m.parent_match_id where j.jackpot_event_id='$jackpotID' and j.status='ACTIVE'  and e.sub_type_id=1 group by j.parent_match_id order by game_order";
 
         $games = $this->rawQueries($games);
 
